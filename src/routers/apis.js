8e2dcbf57d949fbc2375bd6054a3d24113e63587
Ermondel <ermondel@gmail.com>
2020-02-15 21:34:18 +0200

    feat: checks queries

--- src/routers/apis.js
+++ src/routers/apis.js
@@ -4,7 +4,8 @@ const router = new express.Router();
 router.get('/rnd', (req, res) => {
   const content = Math.floor(Math.random() * 100);
 
-  console.log('---', req.headers);
+  console.log('---', req.query);
+  console.log('---', req.params);
 
   res.status(201).send({ content });
 });
