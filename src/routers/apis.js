7d613c4b12ecc127f2cc2d4dc62f3546206a8986
Ermondel <ermondel@gmail.com>
2020-02-15 21:45:29 +0200

    feat: reset checks

--- src/routers/apis.js
+++ src/routers/apis.js
@@ -4,9 +4,6 @@ const router = new express.Router();
 router.get('/rnd', (req, res) => {
   const content = Math.floor(Math.random() * 100);
 
-  console.log('---', req.query);
-  console.log('---', req.params);
-
   res.status(201).send({ content });
 });
 
