0ef0df838dda5da18a956ce7adccec3e1aab4be9
Ermondel <ermondel@gmail.com>
2020-02-15 21:18:27 +0200

    feat: checks headers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const apisRouter = require('./routers/apis');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 app.use(apisRouter);
 
--- src/routers/apis.js
+++ src/routers/apis.js
@@ -4,6 +4,8 @@ const router = new express.Router();
 router.get('/rnd', (req, res) => {
   const content = Math.floor(Math.random() * 100);
 
+  console.log('---', req);
+
   res.status(201).send({ content });
 });
 
