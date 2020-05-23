bb97c4d5db1cd27888746214086cb2f7568ff75e
ermondel <ermondel@gmail.com>
2020-05-23 12:52:56 +0300

    feat: update auth system

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,42 @@
 const auth = (req, res, next) => {
-  const allowedOrigins = process.env.WHITE_LIST.split(';').map(
-    (el) => el + '/'
-  );
-  const referer = req.headers.referer;
+  const userAgent = req.headers['user-agent'];
+  const headerAuth = req.headers.authorization;
+  const envAuthKey = process.env.AUTH_KEY;
+  const whiteList = process.env.WHITE_LIST;
+  const ref = req.headers.referer;
+  const dn = ref
+    ? ref[ref.length - 1] === '/'
+      ? ref.slice(0, -1)
+      : ref
+    : undefined;
+  let errorMessage = '';
 
-  if (allowedOrigins.indexOf(referer) >= 0) {
-    next();
+  // checks
+  if (userAgent && userAgent.indexOf('PostmanRuntime') > -1) {
+    if (!headerAuth) {
+      errorMessage = 'Auth key not found in the header';
+    }
+    if (!envAuthKey) {
+      errorMessage = 'Auth key not found in the env';
+    }
+    if (headerAuth && envAuthKey && headerAuth !== envAuthKey) {
+      errorMessage = 'Auth keys do not match';
+    }
   } else {
-    res.status(401).send({ error: 'Please authenticate.' });
+    if (!whiteList) {
+      errorMessage = 'No whitelist found';
+    }
+    if (whiteList && whiteList.indexOf(dn) < 0) {
+      errorMessage = 'deny: ' + dn;
+    }
+  }
+
+  // result
+  if (errorMessage) {
+    console.log('[auth error]', errorMessage);
+    res.status(401).send({ error: 'Unauthorized' });
+  } else {
+    next();
   }
 };
 
--- src/routers/util.js
+++ src/routers/util.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.get('', (req, res) => {
   res.status(200).send('node api server');
 });
 
-router.get('/rnd', (req, res) => {
-  res.status(200).send({ data: Math.floor(Math.random() * 100) });
+router.get('/ping', auth, (req, res) => {
+  res.status(200).send({ status: 'ok', data: Math.floor(Math.random() * 100) });
 });
 
 module.exports = router;
