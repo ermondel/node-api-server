1c8f8bebe71d6e37a214fee7ac7c85a439a3a489
ermondel <ermondel@gmail.com>
2020-05-23 15:16:25 +0300

    feat: update query system

--- src/routers/apis.js
+++ src/routers/apis.js
@@ -3,70 +3,65 @@ const axios = require('axios');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-router.get('/youtube', auth, async (req, res) => {
-  const youtube = await axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-      ...req.query,
-      key: process.env.YOUTUBE_API,
-    },
-  });
+const APIConfig = {
+  youtube: {
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    key: process.env.YOUTUBE_API,
+    keyParam: 'key',
+  },
+  unsplash: {
+    url: 'https://api.unsplash.com/search/photos',
+    key: 'Client-ID ' + process.env.UNSPLASH_API,
+    keyHeader: 'Authorization',
+  },
+  openweathermap: {
+    url: 'http://api.openweathermap.org/data/2.5/forecast',
+    key: process.env.OPENWEATHERMAP_API,
+    keyParam: 'appid',
+  },
+};
 
-  youtube
-    .get('/search', {
-      params: { q: req.query.q },
-    })
-    .then((response) => {
+router.get('/request/:api', auth, async (req, res) => {
+  const api = req.params.api;
+  let errorMessage = '';
+
+  if (!APIConfig[api]) {
+    errorMessage = 'No configuration found for the API';
+  }
+
+  if (APIConfig[api] && !APIConfig[api].key) {
+    errorMessage = 'The key for the API is not specified in the config';
+  }
+
+  if (errorMessage) {
+    console.log('[api error]', 500, errorMessage);
+    res.status(500).send();
+  } else {
+    const axiosConfig = {
+      params: { ...req.query },
+      headers: {},
+    };
+
+    if (APIConfig[api].keyParam) {
+      const param = APIConfig[api].keyParam;
+      axiosConfig.params[param] = APIConfig[api].key;
+    }
+
+    if (APIConfig[api].keyHeader) {
+      const item = APIConfig[api].keyHeader;
+      axiosConfig.headers[item] = APIConfig[api].key;
+    }
+
+    const request = axios.create(axiosConfig);
+
+    try {
+      const response = await request.get(APIConfig[api].url);
       res.status(200).send(response.data);
-    })
-    .catch((error) => {
+    } catch (error) {
+      console.log('[error]', 500, error.message);
       res.status(500).send();
-    });
-});
-
-router.get('/unsplash', auth, async (req, res) => {
-  const unsplash = await axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-      Authorization: 'Client-ID ' + process.env.UNSPLASH_API,
-    },
-  });
-
-  unsplash
-    .get('/search/photos', {
-      params: {
-        query: req.query.q,
-      },
-    })
-    .then((response) => {
-      if (response.headers['x-ratelimit-remaining'] > 0) {
-        res.status(200).send(response.data);
-      } else {
-        res.status(500).send();
-      }
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-router.get('/openweathermap', auth, async (req, res) => {
-  const openweathermap = await axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5',
-    params: {
-      ...req.query,
-      appid: process.env.OPENWEATHERMAP_API,
-    },
-  });
-
-  openweathermap
-    .get('/forecast')
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
+    }
+  }
 });
 
 module.exports = router;
