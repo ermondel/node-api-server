4740ad92cb47803bf27cdbe3cfa189101f2b3a31
ermondel <ermondel@gmail.com>
2020-05-23 15:52:19 +0300

    feat: update cors check

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ const utilRouter = require('./routers/util');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
-  const allowedOrigins = process.env.WHITE_LIST.split(';');
+app.use(function (req, res, next) {
+  const whiteList = process.env.WHITE_LIST;
   const origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) >= 0) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  if (!whiteList) {
+    console.log('[error]', 'No whitelist found');
+    res.status(500).send();
+  } else {
+    if (whiteList.indexOf(origin) > -1) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+    }
+    next();
   }
-  next();
 });
 
 app.use(express.json());
